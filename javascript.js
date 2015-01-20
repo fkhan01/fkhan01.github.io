c5839115cc5ec584748d9547923ed17f98c4cd47
Faraz Khan <fkhan0485@gmail.com>
2015-01-20 04:01:23 -0500

    index, css, js

--- javascript.js
+++ javascript.js
@@ -3,17 +3,19 @@ $(document).ready(function(){
   $("h1").fadeOut();
    $("#portfolio").fadeOut();
     $("#projects").fadeOut();
-     $("#info").fadeIn(2000);
+     $("#aboutme").fadeOut();
+      $("#info").fadeIn(2000);
       $("#back").show();
-       $("div").animate({height:'350px',width:'400px'},2000);
+       $("div").animate({height:'150px'},2000);
        
   });
   $("#back").click(function(){
    $("h1").fadeIn(2000);
     $("#portfolio").fadeIn();
      $("#projects").fadeIn();
-      $("div").animate({height:'3px',width:'70px'});
+      $("#aboutme").fadeIn();
+      $("div").animate({height:'3px'});
        $("#back").hide();
-        $("#info").hide();
+         $("#info").hide();
   });
 });
