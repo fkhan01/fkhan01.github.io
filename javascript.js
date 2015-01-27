1c51d5c78e0cc84aaadf2822d5734a7e6e6a0be6
Faraz Khan <fkhan0485@gmail.com>
2015-01-27 01:51:09 -0500

    js

--- javascript.js
+++ javascript.js
@@ -26,6 +26,7 @@ $(document).ready(function(){
      $("#projects").fadeOut();
       $("#aboutme").fadeOut();
        $(".container").fadeIn(2000);
+       $(".slider_wrapper").fadeIn(2000);
         $("#back").show(500);
         // $("div").animate({height:'150px'},2000);
    });
