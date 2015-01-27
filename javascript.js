7de989e7e000072abba1eb68a2420369e1cebb25
Faraz Khan <fkhan0485@gmail.com>
2015-01-27 01:00:59 -0500

    js

--- javascript.js
+++ javascript.js
@@ -32,74 +32,109 @@ $(document).ready(function(){
 
 
 
+//1. set ul width 
+//2. image when click prev/next button
 var ul;
-var liItems; 
-var imageWidth;
+var liItems;
 var imageNumber;
+var imageWidth;
+var prev, next;
+var currentPostion = 0;
+var currentImage = 0;
+
 
 function init(){
+  ul = document.getElementById('image_slider');
+  liItems = ul.children;
+  imageNumber = liItems.length;
+  imageWidth = liItems[0].children[0].clientWidth;
+  ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
+  prev = document.getElementById("prev");
+  next = document.getElementById("next");
+  generatePager(imageNumber);
+  //.onclike = slide(-1) will be fired when onload;
+  /*
+  prev.onclick = function(){slide(-1);};
+  next.onclick = function(){slide(1);};*/
+  prev.onclick = function(){ onClickPrev();};
+  next.onclick = function(){ onClickNext();};
+}
 
-    ul = document.getElementById('image_slider');
-    liItems = ul.children;
-    imageNumber = liItems.length;
-    imageWidth = liItems[0].children[0].offsetWidth;
-    // set ul’s width as the total width of all images in image slider.
-    ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
-    slider(ul);
-}
- function slider(ul){ 
-    animate({
-        delay:17,
-        duration: 3000,
-        delta:function(p){return Math.max(0, -1 + 2 * p);},
-        step:function(delta){
-            ul.style.left = '-' + parseInt(currentImage * imageWidth + delta * imageWidth) + 'px';
-    },
-        callback:function(){
-            currentImage++;
-        // if it doesn’t slied to the last image, keep sliding
-            if(currentImage < imageNumber-1){
-                slider(ul);
-        }
-       // if current image it’s the last one, it slides back to the first one
-            else{
-                var leftPosition = (imageNumber - 1) * imageWidth;
-               // after 2 seconds, call the goBack function to slide to the first image 
-                setTimeout(function(){goBack(leftPosition);},2000); 
-                setTimeout(function(){slider(ul);}, 4000);
-            }
-        }
-    });
-}
-function goBack(leftPosition){
-    currentImage = 0; 
-    var id = setInterval(function(){
-        if(leftPosition >= 0){
-            ul.style.left = '-' + parseInt(leftPosition) + 'px';
-            leftPosition -= imageWidth / 10;
-        }
-        else{
-            clearInterval(id);
-        } 
-    }, 17);
-}
 function animate(opts){
-    var start = new Date();
-    var id = setInterval(function(){
-        var timePassed = new Date() - start;
-        var progress = timePassed / opts.duration;
-        if(progress > 1){
-            progress = 1;
-        }
-        var delta = opts.delta(progress);
-        opts.step(delta);
-        if (progress == 1){
-            clearInterval(id);
-           opts.callback();
-         }
-    }, opts.dalay || 17);
+  var start = new Date;
+  var id = setInterval(function(){
+    var timePassed = new Date - start;
+    var progress = timePassed / opts.duration;
+    if (progress > 1){
+      progress = 1;
+    }
+    var delta = opts.delta(progress);
+    opts.step(delta);
+    if (progress == 1){
+      clearInterval(id);
+      opts.callback();
+    }
+  }, opts.delay || 17);
+  //return id;
+}
+
+function slideTo(imageToGo){
+  var direction;
+  var numOfImageToGo = Math.abs(imageToGo - currentImage);
+  // slide toward left
+
+  direction = currentImage > imageToGo ? 1 : -1;
+  currentPostion = -1 * currentImage * imageWidth;
+  var opts = {
+    duration:1000,
+    delta:function(p){return p;},
+    step:function(delta){
+      ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
+    },
+    callback:function(){currentImage = imageToGo;}  
+  };
+  animate(opts);
+}
+
+function onClickPrev(){
+  if (currentImage == 0){
+    slideTo(imageNumber - 1);
+  }     
+  else{
+    slideTo(currentImage - 1);
+  }   
+}
+
+function onClickNext(){
+  if (currentImage == imageNumber - 1){
+    slideTo(0);
+  }   
+  else{
+    slideTo(currentImage + 1);
+  }   
+}
+
+function generatePager(imageNumber){  
+  var pageNumber;
+  var pagerDiv = document.getElementById('pager');
+  for (i = 0; i < imageNumber; i++){
+    var li = document.createElement('li');
+    pageNumber = document.createTextNode(parseInt(i + 1));
+    li.appendChild(pageNumber);
+    pagerDiv.appendChild(li);
+    // add event inside a loop, closure issue.
+    li.onclick = function(i){
+      return function(){
+        slideTo(i);
+      }
+    }(i);
+  } 
+  var computedStyle = document.defaultView.getComputedStyle(li, null);
+  //border width 1px; offsetWidth = 22
+  var liWidth = parseInt(li.offsetWidth);
+  var liMargin = parseInt(computedStyle.margin.replace('px',""));
+  pagerDiv.style.width = parseInt((liWidth + liMargin * 2) * imageNumber) + 'px';
 }
 window.onload = init;
- 
 
   });
