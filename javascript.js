6ba961c99a0acd36bb433a95ad7eabff37fd1a1e
Faraz Khan <fkhan0485@gmail.com>
2015-03-06 15:47:56 -0500

    updates

--- javascript.js
+++ javascript.js
@@ -52,109 +52,45 @@ $(document).ready(function (){
    });
 
 
-//1. set ul width 
-//2. image when click prev/next button
-var ul;
-var liItems;
-var imageNumber;
-var imageWidth;
-var prev, next;
-var currentPostion = 0;
-var currentImage = 0;
+  
+  var slideCount = $('#slider ul li').length;
+  var slideWidth = $('#slider ul li').width();
+  var slideHeight = $('#slider ul li').height();
+  var sliderUlWidth = slideCount * slideWidth;
+  
+  $('#slider').css({ width: slideWidth, height: slideHeight });
+  
+  $('#slider ul').css({ width: sliderUlWidth, marginLeft: - slideWidth });
+  
+    $('#slider ul li:last-child').prependTo('#slider ul');
 
+    function moveLeft() {
+        $('#slider ul').animate({
+            left: + slideWidth
+        }, 200, function () {
+            $('#slider ul li:last-child').prependTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
 
-function init(){
-  ul = document.getElementById('image_slider');
-  liItems = ul.children;
-  imageNumber = liItems.length;
-  imageWidth = liItems[0].children[0].clientWidth;
-  ul.style.width = parseInt(imageWidth * imageNumber) + 'px';
-  prev = document.getElementById("prev");
-  next = document.getElementById("next");
-  generatePager(imageNumber);
-  //.onclike = slide(-1) will be fired when onload;
-  /*
-  prev.onclick = function(){slide(-1);};
-  next.onclick = function(){slide(1);};*/
-  prev.onclick = function(){ onClickPrev();};
-  next.onclick = function(){ onClickNext();};
-}
+    function moveRight() {
+        $('#slider ul').animate({
+            left: - slideWidth
+        }, 200, function () {
+            $('#slider ul li:first-child').appendTo('#slider ul');
+            $('#slider ul').css('left', '');
+        });
+    };
 
-function animate(opts){
-  var start = new Date();
-  var id = setInterval(function(){
-    var timePassed = new Date() - start;
-    var progress = timePassed / opts.duration;
-    if (progress > 1){
-      progress = 1;
-    }
-    var delta = opts.delta(progress);
-    opts.step(delta);
-    if (progress == 1){
-      clearInterval(id);
-      opts.callback();
-    }
-  }, opts.delay || 17);
-  //return id;
-}
+    $('a.control_prev').click(function () {
+        moveLeft();
+    });
 
-function slideTo(imageToGo){
-  var direction;
-  var numOfImageToGo = Math.abs(imageToGo - currentImage);
-  // slide toward left
+    $('a.control_next').click(function () {
+        moveRight();
+    });
 
-  direction = currentImage > imageToGo ? 1 : -1;
-  currentPostion = -1 * currentImage * imageWidth;
-  var opts = {
-    duration:1000,
-    delta:function(p){return p;},
-    step:function(delta){
-      ul.style.left = parseInt(currentPostion + direction * delta * imageWidth * numOfImageToGo) + 'px';
-    },
-    callback:function(){currentImage = imageToGo;}  
-  };
-  animate(opts);
-}
-
-function onClickPrev(){
-  if (currentImage == 0){
-    slideTo(imageNumber - 1);
-  }     
-  else{
-    slideTo(currentImage - 1);
-  }   
-}
-
-function onClickNext(){
-  if (currentImage == imageNumber - 1){
-    slideTo(0);
-  }   
-  else{
-    slideTo(currentImage + 1);
-  }   
-}
-
-function generatePager(imageNumber){  
-  var pageNumber;
-  var pagerDiv = document.getElementById('pager');
-  for (i = 0; i < imageNumber; i++){
-    var li = document.createElement('li');
-    pageNumber = document.createTextNode(parseInt(i + 1));
-    li.appendChild(pageNumber);
-    pagerDiv.appendChild(li);
-    // add event inside a loop, closure issue.
-    li.onclick = function(i){
-      return function(){
-        slideTo(i);
-      };
-    }(i);
-  }
-  var computedStyle = document.defaultView.getComputedStyle(li, null);
-  //border width 1px; offsetWidth = 22
-  var liWidth = parseInt(li.offsetWidth);
-  var liMargin = parseInt(computedStyle.margin.replace('px',""));
-  pagerDiv.style.width = parseInt((liWidth + liMargin * 2) * imageNumber) + 'px';
-}
+});   
 window.onload = init;
 
 });
